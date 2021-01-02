1983ebd1cd625ea00dd8ca40282fbeacb599b7c8
Kay Lin <i@v2bv.net>
2021-01-01 18:41:25 -0800

    main: Make shellcheck happy about this script
    Signed-off-by: Kay Lin <i@v2bv.net>

--- main.sh
+++ main.sh
@@ -1,14 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Copyright © 2018 Kay <RedL0tus@users.noreply.github.com>
+# Copyright © 2018-2020 Kay <i@v2bv.net>
 # This work is free. You can redistribute it and/or modify it under the
 # terms of the Do What The Fuck You Want To Public License, Version 2,
 # as published by Sam Hocevar. See the LICENSE file for more details.
 
-set -e;
+set -euo pipefail;
 
 function GET_PERCENTAGE {
-	local CURRENT_YEAR=$(date +%Y);
+	local CURRENT_YEAR;
+	CURRENT_YEAR=$(date +%Y);
 	if [ $((CURRENT_YEAR % 400)) -eq 0 ]; then
 		local TOTAL_DAYS=366;
 	elif [ $((CURRENT_YEAR % 100)) -eq 0 ]; then
@@ -19,39 +20,43 @@ function GET_PERCENTAGE {
 		local TOTAL_DAYS=365;
 	fi
 	CURRENT_DAY=$(echo "$(date +%j) + 0" | bc);
-	echo $(($CURRENT_DAY*100/$TOTAL_DAYS));
+	echo $((CURRENT_DAY*100/TOTAL_DAYS));
 }
 
 function DISPLAY {
-	local PERCENTAGE=$(GET_PERCENTAGE);
-	local FILLED=$(($LENGTH*$PERCENTAGE/100));
-	local BLANK=$(($LENGTH-$FILLED));
+	local PERCENTAGE;
+	PERCENTAGE=$(GET_PERCENTAGE);
+	local FILLED=$((LENGTH*PERCENTAGE/100));
+	local BLANK=$((LENGTH-FILLED));
 	local BAR="";
-	for ((i=0;i<$FILLED;i++)) {
-		BAR=${BAR}"▓";
+	for ((i=0;i<FILLED;i++)) {
+		BAR="${BAR}▓";
 	}
-	for ((i=0;i<$BLANK;i++)) {
-		BAR=${BAR}"░";
+	for ((i=0;i<BLANK;i++)) {
+		BAR="${BAR}░";
 	}
-	BAR=${BAR}" "${PERCENTAGE}"%";
-	echo $BAR;
+	echo "${BAR} ${PERCENTAGE}%";
 }
 
 function MAIN {
+	local BAR_NOW;
 	local BAR="";
-	local BAR_NOW=$(DISPLAY);
+	BAR_NOW=$(DISPLAY);
 	if [ -f "$WORKDIR"/bar ]; then
 		BAR=$(cat "$WORKDIR"/bar);
 	fi
 	echo ">>> Bot started.";
 	while true; do
 		BAR_NOW=$(DISPLAY);
-		if [ "$BAR" != "$BAR_NOW" ]; then
-			curl -X POST "https://api.telegram.org/bot${API_TOKEN}/sendMessage" -d "chat_id=${CHAT_ID}&text=${BAR_NOW}";
-			echo $BAR_NOW > "$WORKDIR"/bar;
-			BAR=$BAR_NOW;
+		if [ "$BAR" == "$BAR_NOW" ]; then
+			sleep 100;
+			continue;
 		fi
-		sleep 100;
+		curl -X POST \
+			"https://api.telegram.org/bot${API_TOKEN}/sendMessage" \
+			-d "chat_id=${CHAT_ID}&text=${BAR_NOW}";
+		echo "$BAR_NOW" > "$WORKDIR"/bar;
+		BAR="$BAR_NOW";
 	done
 }
 
